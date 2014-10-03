3e12b244886b142ca69ac8e857bbff431857458a
Randy Carlisle <randy@pd-quick.com>
2014-10-03 05:33:22 +0000

    Update Gemfile.lock for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -8,23 +8,18 @@ Rails.application.routes.draw do
     delete 'logout' => :destroy
   end
   
-  resources :users
-
-  resources :orders
-
-  resources :line_items
-
-  resources :carts
-
-  get 'store/index'
-
-  resources :products do 
-    get :who_bought, :on => :member
+  scope '(:locale)' do
+    resources :users
+    resources :orders
+    resources :line_items
+    resources :carts
+  #   get 'store/index'
+    resources :products do 
+      get :who_bought, :on => :member
+    end
+    #create a store_path variable:
+    root :to => 'store#index', :as => 'store'
   end
-  
-  #create a store_path variable:
-  root :to => 'store#index', :as => 'store'
-  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
